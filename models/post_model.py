0561523aed3a52ef846cd6124e76d04beb1b49ea
Udean777 <uaart123@gmail.com>
2024-07-23 10:37:17 +0700

    just commit

--- models/post_model.py
+++ models/post_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TEXT, VARCHAR, Column, ForeignKey
+from sqlalchemy import TEXT, VARCHAR, Column, DateTime, ForeignKey, func
 from sqlalchemy.orm import relationship
 from models.base_model import Base
 
@@ -9,6 +9,8 @@ class Post(Base):
     image_url = Column(TEXT)
     caption = Column(VARCHAR(255))
     user_id = Column(TEXT, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
     user = relationship("UserModel", back_populates="posts")
     saved_posts = relationship("SavedModel", back_populates="post")
--- models/saved_model.py
+++ models/saved_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import TEXT, Column, ForeignKey
+from sqlalchemy import TEXT, Column, DateTime, ForeignKey, func
 from models.base_model import Base
 from sqlalchemy.orm import relationship
 
@@ -8,6 +8,8 @@ class SavedModel(Base):
     id = Column(TEXT, primary_key=True)
     post_id = Column(TEXT, ForeignKey("posts.id"))
     user_id = Column(TEXT, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
     post = relationship("Post")
     user = relationship("UserModel", back_populates="saved_posts")
